67e404f910230ea9c70d99684b8d9cb7c8e80542
alex-varga14 <57467835+alex-varga14@users.noreply.github.com>
2022-06-03 18:11:52 -0600

    header file model for option

---
+++ EuropeanOption.hpp
@@ -0,0 +1,52 @@
+
+
+class EuropeanOption 
+{
+ private:
+	// Initialize all default values
+	void init(); 
+	void copy(const EuropeanOption& src);
+	
+	// Kernel functions for option calc
+	double CallPrice() const;
+	double PutPrice() const;
+	double CallDelta() const;
+	double PutDelta() const;
+	
+ public:
+	// Member data 
+	double r;	// Interest Rate
+	double sig;	// Volatility
+	double K;	// Strike Price
+	double T; 	// Expiry Date
+	double U; 	// Current Underlying Price
+	double b; 	// Cost of carry
+	
+	string optType; 	// Option name - call or put
+	
+	// Rule of Big 3
+	
+	// Constructors
+	EuropeanOption();
+	EuropeanOption(const EuropeanOption& option2); // Cpy CTOR
+	EuropeanOption(const string& optionType); 	// Create option
+	
+	// Destructor
+	virtual ~EuropeanOption();
+	
+	// Assignment Operator
+	EuropeanOption& operator =(const EuropeanOption& src);
+	
+	// Func to calc option price and Delta
+	double Price() const;
+	double Delta() const;
+	
+	// Modifier
+	void toggle(); // Change opt type 
+ 
+ 
+};
+	
+	
+	
+	
